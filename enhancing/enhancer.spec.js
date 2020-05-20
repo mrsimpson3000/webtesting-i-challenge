38339c9c8ac86fa34a8821542dbd38ad4ed16540
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-20 17:52:16 -0400

    Test for repair function works. It took me a minute to realize that you have to repair the durability of both objects instead of trying to compare whole objects.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,23 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+describe("enhancer", () => {
+  describe("repair()", () => {
+    it("should set the item.durability to 100", () => {
+      const wornItem = {
+        name: "Rusty Iron Sword",
+        durability: 1,
+        enhancement: 0,
+      };
+
+      const expectedItem = {
+        name: "Rusty Iron Sword",
+        durability: 100,
+        enhancement: 0,
+      };
+
+      const actualItem = enhancer.repair(wornItem);
+
+      expect(actualItem.durability).toBe(expectedItem.durability);
+    });
+  });
+});
