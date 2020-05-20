86b520cfa7572f2798b156841376353b59237f99
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-20 17:26:00 -0400

    All functions for MVP written. Yet to write any testing.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,35 @@ module.exports = {
 };
 
 function succeed(item) {
+  if (item.enhancement < 20) {
+    // enhance +1
+    item.enhancement += 1;
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    if (item.durability < 5) {
+      item.durability = 0;
+    } else {
+      item.durability -= 5;
+    }
+  } else if (item.enhancement >= 15) {
+    if (item.enhancement > 16) {
+      item.enhancement -= 1;
+    }
+    if (item.durability < 10) {
+      item.durability = 0;
+    } else {
+      item.durability -= 10;
+    }
+  }
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
