f2db6e220a0a32a4a0c08455e701b522fb805706
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-20 18:36:07 -0400

    Second set of tests done and working. It can sometimes take some time to figure out why tests fail.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -20,4 +20,56 @@ describe("enhancer", () => {
       expect(actualItem.durability).toBe(expectedItem.durability);
     });
   });
+
+  describe("succeed()", () => {
+    it("should add 1 to item.enhancement if it is less than 20", () => {
+      const itemOne = {
+        name: "Rusty Iron Sword",
+        durability: 72,
+        enhancement: 20,
+      };
+
+      const itemTwo = {
+        name: "Mithril Sword",
+        durability: 17,
+        enhancement: 0,
+      };
+
+      const itemThree = {
+        name: "Mithril Sword",
+        durability: 59,
+        enhancement: 15,
+      };
+
+      const expectedItemOne = {
+        name: "Rusty Iron Sword",
+        durability: 72,
+        enhancement: 20,
+      };
+
+      const expectedItemTwo = {
+        name: "Mithril Sword",
+        durability: 17,
+        enhancement: 1,
+      };
+
+      const expectedItemThree = {
+        name: "Mithril Sword",
+        durability: 59,
+        enhancement: 16,
+      };
+
+      const actualItemOne = enhancer.succeed(itemOne);
+
+      const actualItemTwo = enhancer.succeed(itemTwo);
+
+      const actualItemThree = enhancer.succeed(itemThree);
+
+      expect(expectedItemOne.enhancement).toBe(actualItemOne.enhancement);
+
+      expect(expectedItemTwo.enhancement).toBe(actualItemTwo.enhancement);
+
+      expect(expectedItemThree.enhancement).toBe(actualItemThree.enhancement);
+    });
+  });
 });
